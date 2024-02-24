c3997f9e221fe6755125d09336dbe35ce8f358c3
Ethan Chen <acuratnam@gmail.com>
2024-02-25 00:44:21 +0800

    <v0.0.2>

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 export default function Home() {
+  const mintByAA = async () => {
+    try {
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <main className="w-full min-h-screen m-auto  bg-[url('~/public/bg.png'),_url('~/public/splashedwater.png')] bg-cover bg-center flex flex-col justify-center items-center">
         <div className="w-full h-full m-auto bg-splashedwater bg-cover bg-center flex flex-col justify-center items-center">
           TEST
+          <div>
+            <button onClick={() => mintByAA()}>Mint By AA</button>
+          </div>
         </div>
       </main>
     </>
