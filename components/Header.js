0f8bd9b15eaac028dc95fb81d36e06403a5f91da
Ethan Chen <acuratnam@gmail.com>
2024-02-24 16:41:34 +0800

    aa test

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,55 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import React, { useEffect, useState } from "react";
 import { useAccount, useBalance } from "wagmi";
-import Decimal from "decimal.js";
 import Image from "next/image";
 import NextLink from "next/link";
 import { Link } from "@chakra-ui/react";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbSeparator,
-  Button,
-  Stack,
-} from "@chakra-ui/react";
+//
+import { ParticleNetwork } from "@particle-network/auth";
+import { EthereumSepolia } from "@particle-network/chains";
+import { RiTwitterXLine } from "react-icons/ri";
+import { FaGoogle } from "react-icons/fa";
+//
+const config = {
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  clientKey: process.env.REACT_APP_CLIENT_KEY,
+  appId: process.env.REACT_APP_APP_ID,
+};
 
+const particle = new ParticleNetwork({
+  ...config,
+  chainName: EthereumSepolia.name,
+  chainId: EthereumSepolia.id,
+  wallet: { displayWalletEntry: true, uiMode: "dark" },
+});
+
+particle.setERC4337(true);
+
+const handleLogin = async (preferredAuthType) => {
+  try {
+    const user = !particle.auth.isLogin()
+      ? await particle.auth.login({ preferredAuthType })
+      : particle.auth.getUserInfo();
+    setUserInfo(user);
+  } catch (error) {
+    console.error(error);
+    setUserInfo(null);
+  }
+};
+
+const handleLogout = async () => {
+  try {
+    await particle.auth.logout();
+    setUserInfo(null);
+  } catch (error) {
+    console.error(error);
+    setUserInfo(null);
+  }
+};
+//
 export default function Header() {
-  const imageStyle = {
-    borderRadius: "50%",
-    border: "1px solid #fff",
-  };
+  const [userInfo, setUserInfo] = useState(null);
+
   return (
     <nav className="bg-heroImage bg-cover bg-center height: 60px p-3 border-b-2 flex flex-row justify-between items-center ">
       <div className="flex-none gap-4 items-center">
@@ -34,53 +65,26 @@ export default function Header() {
             Dragon Mint
           </h1>
         </Link>
-        <Breadcrumb className="py-4 px-4 float-left">
-          <BreadcrumbItem className="bg-accent opacity-80 text-black rounded-l-lg m1 py-1 px-1 hover:bg-indigo-500 opacity-100 hover: text-black transition-all duration-300 ease-in-out">
-            <BreadcrumbLink href="/">
-              <h1 className="py-2 px-2 font-bold text-2xl float-left">Home</h1>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbItem className="bg-accent opacity-80 text-black rounded-3x2 m1 py-1 px-1 hover:bg-indigo-500 opacity-100 hover: text-black transition-all duration-300 ease-in-out">
-            <BreadcrumbLink href="/create-nft">
-              <h1 className="py-2 px-2 font-bold text-2xl float-left">
-                Create NFT
-              </h1>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbItem className="bg-accent opacity-80 text-black rounded-r-lg m1 py-1 px-1 hover:bg-indigo-500 opacity-100 hover: text-black transition-all duration-300 ease-in-out">
-            <BreadcrumbLink href="/nft-marketplace">
-              <h1 className="py-2 px-2 font-bold text-2xl float-left">
-                NFT Marketplace
-              </h1>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-        </Breadcrumb>
       </div>
+
+      <div>
+        {!userInfo ? (
+          <div>
+            <button onClick={() => handleLogin("google")}>
+              <FaGoogle />
+            </button>
+            <button onClick={() => handleLogin("twitter")}>
+              <RiTwitterXLine />
+            </button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={() => handleLogout()}>Logout</button>
+          </div>
+        )}
+      </div>
+
       <div className="flex-none gap-4 items-center">
-        <Stack direction="row" spacing={10}>
-          <NextLink href="/profile" passHref>
-            <Button>
-              <Image
-                className="float-left"
-                borderdadius="full"
-                src="/profile.png"
-                alt="profile logo"
-                height={60}
-                width={60}
-                style={imageStyle}
-              />
-            </Button>
-          </NextLink>
-          <Button onClick={() => alert("openCartModal")}>
-            <Image
-              className="float-left"
-              src="/shopping-cart.png"
-              alt="shopping cart logo"
-              height={60}
-              width={60}
-            />
-          </Button>
-        </Stack>
         <ConnectButton className="p-5 border-b-8  flex flex-row justify-between items-center float-right" />
       </div>
     </nav>
--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,12 @@
 import "@/styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
-
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { configureChains, createConfig, sepolia, WagmiConfig } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import Head from "next/head";
 import Header from "../components/Header";
-import { AiFillGithub } from "react-icons/ai";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-
-//
-import { LockClosedIcon, LockOpenIcon } from "@heroicons/react/24/outline";
-import LockScreen from "react-lock-screen";
-//
 
 const { chains, publicClient } = configureChains(
   [sepolia],
@@ -39,18 +32,6 @@ const client = new ApolloClient({
 });
 
 export default function App({ Component, pageProps }) {
-  const getLockScreenUi = (setLock) => {
-    return (
-      <div className="react-lock-screen__ui">
-        <LockClosedIcon className="h-8 w-8 text-gray-700" />
-        <p>Just to be safe, we locked the screen</p>
-        <button onClick={() => setLock(false)}>
-          <LockOpenIcon className="h-8 w-8 text-gray-700" />
-        </button>
-      </div>
-    );
-  };
-
   return (
     <WagmiConfig config={wagmiConfig}>
       <RainbowKitProvider chains={chains}>
@@ -60,25 +41,10 @@ export default function App({ Component, pageProps }) {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/2024 year of the dragon.png" />
         </Head>
-        <LockScreen timeout={600000} ui={getLockScreenUi}>
-          <Header />
-          <ApolloProvider client={client}>
-            <Component {...pageProps} />
-          </ApolloProvider>
-          <div className="bg-accent w-full h-full">
-            <div className="max-w-screen-xl mx-auto py-6 flex justify-between items-center text-light">
-              <p className="text-xs">WAGMI © 2024</p>
-              <a
-                href="https://github.com/ethanysc-git/dragon-mint"
-                target="_blank"
-              >
-                <AiFillGithub />
-              </a>
-
-              <div className="flex items-center gap-10 mr-4"></div>
-            </div>
-          </div>
-        </LockScreen>
+        <Header />
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
       </RainbowKitProvider>
     </WagmiConfig>
   );
